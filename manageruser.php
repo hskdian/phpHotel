6d6c138185e479b49e6df0c1542f4d8a1274218b
hskdian <ian.yiyuan.lin@gmail.com>
2017-04-02 11:51:06 -0700

    form cards for filter and project

---
+++ manageruser.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>BookingPals</title>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootcards/1.0.0/css/bootcards-desktop.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+  <script src="https://use.fontawesome.com/fbc1a80091.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="home.php" class="navbar-brand">BookingPals</a>
+
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav">
+          <li><a href="manager.php">Manager View</a></li>
+          <li class = "active"><a href="manageruser.php">Manage Users</a></li>
+          <li><a href="#">Manage Reservations</a></li>
+
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Signup <i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
+          <li><a href="#">Login <i class="fa fa-user" aria-hidden="true"></i></a></li>
+          <li><a href="#">Manager <i class="fa fa-user-secret" aria-hidden="true"></i></a></li>
+        </div>
+      </div>
+    </ul>
+  </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="panel panel-default">
+          <div class="panel-heading clearfix">
+            <h3 class="panel-title pull-left">Find User</h3>
+            <div class="btn-group pull-right">
+              <button class="btn btn-danger">
+                <i class="fa fa-times"></i>
+                Cancel
+              </button>
+              <button class="btn btn-success">
+                <i class="fa fa-check"></i>
+                Find
+              </button>
+            </div>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">Show in Table</label>
+                <div class="col-xs-9">
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description">Show Credit Card</span>
+                  </label>
+
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description">Show GuestID</span>
+                  </label>
+
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description">Show Username</span>
+                  </label>
+
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description">Show Name</span>
+                  </label>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">Credit Card Numner</label>
+                <div class="col-xs-9">
+                  <input type="text" class="form-control" value="John Smith">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">GuestID</label>
+                <div class="col-xs-9">
+                  <select class="form-control">
+                    <option>Designer</option>
+                    <option selected>Developer</option>
+                    <option>Salesman</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">Username</label>
+                <div class="col-xs-9">
+                  <input type="text" class="form-control" value="John Smith">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">Show Name</label>
+                <div class="col-xs-9">
+                  <input type="text" class="form-control" value="John Smith">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-xs-3 control-label">Show Points (Only avaiable for showing all Users)</label>
+                <div class="col-xs-9">
+                  <input type="text" class="form-control" value="John Smith">
+                </div>
+              </div>
+
+            </form>
+          </div>
+        </div>
+
+        <div style="background:transparent !important" class="jumbotron">
+          <div id="responsecontainer" align="center" color = "grey">
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootcards/1.0.0/js/bootcards.min.js"></script>
+    <script type="text/javascript" src ="manager.js"></script>
+  </body>
+  </html>
